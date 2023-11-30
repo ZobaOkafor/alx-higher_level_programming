2809409387cdbd845a5de83e61d10e8920345964
ZobaOkafor <judithokafor30@gmail.com>
2023-11-30 13:40:29 -0800

    Magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,8 +5,8 @@
 
 def magic_calculation(a, b):
     if a < b:
-        add = __import__('magic_calculation_102').add 
-        sub = __import__('magic_calculation_102').sub
+        add = __import__ magic_calculation_102.add 
+        sub = __import__ magic_calculation_102.sub
         c = add(a, b)
 
         for i in range(4, 6):
