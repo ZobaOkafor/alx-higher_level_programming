88fd723dcf7b80e54fef6e8bb13c7ccf65c6e1aa
ZobaOkafor <judithokafor30@gmail.com>
2023-11-30 00:18:03 -0800

    Print all the names defined in hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Prints all the names in hidden_4.pyc"""
+
+
+if __name__ == "__main__":
+    import hidden_4
+
+    hidden_names = hidden_4
+    noms = dir(hidden_names)
+    
+    for nom in noms:
+        if nom[:2] != "__":
+            print(nom)
