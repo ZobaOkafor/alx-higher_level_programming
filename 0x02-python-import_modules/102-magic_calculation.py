29aad637201c72e4af8ae35c6ac8cdb55dc432bf
ZobaOkafor <judithokafor30@gmail.com>
2023-11-30 13:44:41 -0800

    Magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,10 +3,12 @@
 """Magic calculation that replicates the workings of provided bytecode"""
 
 
+import magic_calculation_102
+
 def magic_calculation(a, b):
     if a < b:
         add = magic_calculation_102.add 
-        sub = magic_calculation_102.sub
+        sub = magic_calculation_101.sub
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
