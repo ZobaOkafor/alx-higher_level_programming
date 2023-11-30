297eb99dc890b9efbe5bd6ceb7e059b3b0d20c65
ZobaOkafor <judithokafor30@gmail.com>
2023-11-30 13:48:05 -0800

    Magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,11 +6,11 @@
 import magic_calculation_102
 
 def magic_calculation(a, b):
-    add = magic_calculation_102.add 
-    sub = magic_calculation_101.sub
+    from magic_calculation_101 import add sub
 
     if a < b:
         c = add(a, b)
+
         for i in range(4, 6):
             c = add(c, i)
         return (c)
