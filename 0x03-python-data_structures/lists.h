0562841e56ab1b32e7becef590eab554fd0afe86
ZobaOkafor <judithokafor30@gmail.com>
2023-12-03 14:37:27 -0800

    CPython Python lists

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 
 /**
@@ -25,5 +26,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
+
 
 #endif
