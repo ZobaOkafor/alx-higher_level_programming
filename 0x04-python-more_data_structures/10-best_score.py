36b4e0cf84b146811a3d36682c95c8935048dd89
ZobaOkafor <judithokafor30@gmail.com>
2023-12-06 02:03:06 -0800

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Returns a key with the biggest integer value"""
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+
+    best_key = max(a_dictionary, key=a_dictionary.get)
+
+    return (best_key)
