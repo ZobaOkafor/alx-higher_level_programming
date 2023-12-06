4a1cadd8d19df0adf682f97be52234bb26873c60
ZobaOkafor <judithokafor30@gmail.com>
2023-12-06 01:03:47 -0800

    Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Returns a set of all elements present in only one set"""
+
+
+def only_diff_elements(set_1, set_2):
+    diff_set = set_1.symmetric_difference(set_2)
+
+    return (diff_set)
