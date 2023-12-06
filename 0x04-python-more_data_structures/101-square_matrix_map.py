903020bdebaa2fa9877d6708c4e636fed0cd86fb
ZobaOkafor <judithokafor30@gmail.com>
2023-12-06 13:35:40 -0800

    Squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-"""Computes the square value of all integers of a matrix using map"""
-
-
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    return (list(map(lambda row: list(map(lambda x: x**2, row)), matrix)))
