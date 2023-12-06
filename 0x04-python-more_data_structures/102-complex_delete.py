83e350c6ffaacdede9805ef7162787df15cde51e
ZobaOkafor <judithokafor30@gmail.com>
2023-12-06 14:04:52 -0800

    Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -7,6 +7,6 @@ def complex_delete(a_dictionary, value):
     keys_to_del = [key for key, val in a_dictionary.items() if val == value]
 
     for key in keys_to_del:
-            a_dictionary.pop(key)
+        a_dictionary.pop(key)
 
     return (a_dictionary)
