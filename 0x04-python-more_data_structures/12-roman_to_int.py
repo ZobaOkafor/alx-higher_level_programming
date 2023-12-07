af85575e18f7bd7eb2e7fc4c10e857dd205ec3c4
ZobaOkafor <judithokafor30@gmail.com>
2023-12-06 16:10:09 -0800

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -22,23 +22,3 @@ def roman_to_int(roman_string):
         prev_value = value
 
     return (result)
-
-
-
-roman_to_int = __import__('12-roman_to_int').roman_to_int
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
