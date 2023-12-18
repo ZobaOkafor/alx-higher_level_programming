7a4c84fcf1ac1fd5b846b342553b8b916180326e
ZobaOkafor <judithokafor30@gmail.com>
2023-12-18 09:52:56 -0800

    Raise exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -4,4 +4,11 @@
 
 
 def raise_exception():
-    raise TypeError
+    raise TypeError("Exception raised")
+
+raise_exception = __import__('5-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
