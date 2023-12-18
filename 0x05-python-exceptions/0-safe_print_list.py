c79f7ce3d60210beccaa215adbf6264dd9d56c95
ZobaOkafor <judithokafor30@gmail.com>
2023-12-18 07:08:35 -0800

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Prints x elements of a list"""
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print()
+        return (count)
+    except IndexError:
+        print()
+        return (count)
