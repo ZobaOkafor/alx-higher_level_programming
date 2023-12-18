7e013a93bcae6bd98095a9ac9b0b60eb3548d970
ZobaOkafor <judithokafor30@gmail.com>
2023-12-18 09:17:47 -0800

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Divides 2 integers and prints the result"""
+
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
