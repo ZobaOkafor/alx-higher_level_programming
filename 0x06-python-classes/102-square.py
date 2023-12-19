58df712da998f1f0eb13b9ecf905be7efd083bc3
ZobaOkafor <judithokafor30@gmail.com>
2023-12-19 12:49:16 -0800

    Compare 2 squares

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -14,12 +14,18 @@ class Square:
         size() -> float or int: Retrieve the size of the square.
         size(value) -> None: Set the size of the square.
         area() -> float or int: Calculate the area of the square.
-        __eq__(other) -> bool: Compare two squares for equality based on area.
-        __ne__(other) -> bool: Compare two squares for inequality based on area.
-        __lt__(other) -> bool: Compare if one square is less than another based on area.
-        __le__(other) -> bool: Compare if one square is less than or equal to another based on area.
-        __gt__(other) -> bool: Compare if one square is greater than another based on area.
-        __ge__(other) -> bool: Compare if one square is greater than or equal to another based on area.
+        __eq__(other) -> bool: Compare two squares for equality
+        based on area.
+        __ne__(other) -> bool: Compare two squares for inequality
+        based on area.
+        __lt__(other) -> bool: Compare if one square is less than
+        another based on area.
+        __le__(other) -> bool: Compare if one square is less than
+        or equal to another based on area.
+        __gt__(other) -> bool: Compare if one square is greater than
+        another based on area.
+        __ge__(other) -> bool: Compare if one square is greater than
+        or equal to another based on area.
     """
 
     def __init__(self, size=0):
@@ -108,13 +114,15 @@ class Square:
 
     def __le__(self, other):
         """
-        Compare if one square is less than or equal to another based on area.
+        Compare if one square is less than or equal to another
+        based on area.
 
         Args:
             other (Square): The square to compare against.
 
         Returns:
-            bool: True if area is less than or equal to the other square, False otherwise.
+            bool: True if area is less than or equal to the other
+            square, False otherwise.
         """
         return (self.area() <= other.area())
 
@@ -126,18 +134,21 @@ class Square:
             other (Square): The square to compare against.
 
         Returns:
-            bool: True if area is greater than the other square, False otherwise.
+            bool: True if area is greater than the other square,
+            False otherwise.
         """
         return (self.area() > other.area())
 
     def __ge__(self, other):
         """
-        Compare if one square is greater than or equal to another based on area.
+        Compare if one square is greater than or equal to another
+        based on area.
 
         Args:
             other (Square): The square to compare against.
 
         Returns:
-            bool: True if area is greater than or equal to the other square, False otherwise.
+            bool: True if area is greater than or equal to the
+            other square, False otherwise.
         """
         return (self.area() >= other.area())
