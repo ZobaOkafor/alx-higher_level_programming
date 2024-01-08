654ed66019680816af048094252019695a7ebeeb
ZobaOkafor <judithokafor30@gmail.com>
2024-01-07 23:17:04 -0800

    TDD

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,8 +7,14 @@ def matrix_divided(matrix, div):
     Divides all elements of a matrix by a given divisor.
 
     Args:
-    - matrix: list of lists of integers or floats
-    - div: number (integer or float) to divide the matrix elements by
+    - matrix: list of lists of integers or floats.
+    - div: number (integer or float) to divide the matrix elements by.
+
+    Raises:
+    - TypeError: If the matrix contains non-numbers.
+    - TypeError: If the matrix contains rows of different sizes.
+    - TypeError: if div is not an int or float.
+    - ZeroDivisionError: If div is 0.
 
     Returns:
     - new matrix with elements rounded to 2 decimal places
