b5225f48e50f2151d1b0c3c6e1a4c5a233e931d4
ZobaOkafor <judithokafor30@gmail.com>
2024-01-09 07:47:10 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,12 +8,11 @@ from os.path import exists
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
-filename = "add_item.json"
 
 # Check if the file exists
-if exists(filename):
+if exists("add_item.json"):
     # Load existing data from the file
-    my_list = load_from_json_file(filename)
+    my_list = load_from_json_file("add_item.json")
 else:
     # Create a new list if the file doesn't exist
     my_list = []
@@ -22,4 +21,4 @@ else:
 my_list.extend(argv[1:])
 
 # Save the updated list to the file
-save_to_json_file(my_list, filename)
+save_to_json_file(my_list, "add_item.json")
