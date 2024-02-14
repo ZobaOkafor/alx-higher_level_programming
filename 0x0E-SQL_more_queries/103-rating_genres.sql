8ce567ab66dbec3d2614a187ac93081c716cdce7
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:30:21 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- Script to list all genres in hbtn_0d_tvshows_rate by their rating.
 
-SELECT tv_genres.title, SUM(tv_show_ratings.rate) rating
+SELECT title, SUM(tv_show_ratings.rate) rating
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_genres.title
+GROUP BY title
 ORDER BY rating DESC;
