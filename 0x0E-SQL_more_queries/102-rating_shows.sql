a5f95def51be6fb5f2fcbc8ae03779d994632c61
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:16:22 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Script to list all shows from hbtn_0d_tvshows_rate by their rating.
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) rating_sum
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
