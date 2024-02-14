d051e2e39a82eb11c84662163d83676b83ce6a39
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 09:20:49 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,4 +7,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege to user_0d_2 in database hbtn_0d_2.
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
