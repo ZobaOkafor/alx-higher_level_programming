6704f824886a9b6503a417741e06e03cf6ce0f7b
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:05:19 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -15,9 +15,9 @@ SELECT name
 FROM tv_genres
 WHERE name NOT IN (
     SELECT tv_genres.name
-    FROM tv_shows
-    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    FROM tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
     WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY name ASC;
