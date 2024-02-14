137605ef5e18bc4d8f5a4b645f5b9d0ef8593263
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 09:45:02 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,5 @@
 -- Script to list all shows without a genre linked in the database hbtn_0d_tvshows.
 
--- Use the database hbtn_0d_tvshows.
-USE hbtn_0d_tvshows;
-
--- Select shows without a linked genre.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
