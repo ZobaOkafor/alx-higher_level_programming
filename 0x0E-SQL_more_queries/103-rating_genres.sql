95f4039b6f141f40eb78c01380eb6000cebe3ea8
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:27:29 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,13 +1,7 @@
 -- Script to list all genres in hbtn_0d_tvshows_rate by their rating.
 
--- Use the database hbtn_0d_tvshows_rate.
-USE hbtn_0d_tvshows_rate;
-
--- Select genres and calculate the sum of ratings for each genre.
-SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS rating_sum
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+SELECT tv_genres.title, SUM(tv_show_ratings.rate) rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.tv_show_id = tv_shows.id
+GROUP BY tv_genres.title
+ORDER BY rating DESC;
