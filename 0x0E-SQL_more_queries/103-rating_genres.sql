6293b0691f14a0cf24876ce7a92689925c673ee2
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:29:07 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_genres.title, SUM(tv_show_ratings.rate) rating
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.tv_show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_genres.title
 ORDER BY rating DESC;
