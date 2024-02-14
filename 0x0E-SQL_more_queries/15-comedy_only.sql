62eb3562880db9ddc354a16d4be17542b645ea7c
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 09:57:13 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,5 @@
 -- Script to list all Comedy shows in the database hbtn_0d_tvshows.
 
--- Use the database hbtn_0d_tvshows.
-USE hbtn_0d_tvshows;
-
--- Select Comedy shows.
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
