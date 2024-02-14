481706354324ab1fd015130bb9c2981e21270b94
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:40:07 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,9 @@
--- Scritp to list all shows from hbtn_0d_tvshows_rate by their rating
+-- Script to list all genres in hbtn_0d_tvshows_rate by their rating
 
-SELECT title, SUM(tv_show_ratings.rate) 'rating'
-FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY title
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
