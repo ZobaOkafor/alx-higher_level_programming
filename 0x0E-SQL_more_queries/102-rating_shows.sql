7c6136f6887f6cf5d3a7011fb0c8c50659e0aee1
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:13:19 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,7 @@
 -- Script to list all shows from hbtn_0d_tvshows_rate by their rating.
 
--- Use the database hbtn_0d_tvshows_rate.
-USE hbtn_0d_tvshows_rate;
-
--- Select shows and calculate the sum of ratings for each show.
 SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
