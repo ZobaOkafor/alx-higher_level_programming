6ea4a42167e8ebb03bbacdffe9831d79086e9ba8
ZobaOkafor <judithokafor30@gmail.com>
2024-02-14 10:00:43 -0800

    SQL more queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,7 @@
 -- Script to list all shows and their genres in the database hbtn_0d_tvshows.
 
--- Use the database hbtn_0d_tvshows.
-USE hbtn_0d_tvshows;
-
--- Select all shows and their genres.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
