6d2b0d0bf080834dae1850699d99ce45534e686b
ZobaOkafor <judithokafor30@gmail.com>
2024-03-11 14:41:55 -0700

    Compute and print a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+
+const input = parseInt(process.argv[2], 10);
+
+console.log(factorial(input));
