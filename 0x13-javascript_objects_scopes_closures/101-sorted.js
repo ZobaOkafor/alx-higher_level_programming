0a0ed32cba009b23937eee14a87786ada74bfb34
ZobaOkafor <judithokafor30@gmail.com>
2024-03-12 05:03:30 -0700

    JavaScript objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
 
-const dict = require('./101-data').dict;
+const { dict } = require('./101-data');
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (!(occurrences in newDict)) {
+    newDict[occurrences] = [];
   }
-  newDict[valsUniq[j]] = list;
+  newDict[occurrences].push(userId);
 }
+
 console.log(newDict);
