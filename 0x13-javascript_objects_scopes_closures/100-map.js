686ae42b7480b15c95f83495d32ad98da9385bb6
ZobaOkafor <judithokafor30@gmail.com>
2024-03-12 04:56:01 -0700

    JavaScript objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,10 @@
 
 const { initialList } = require('./100-data');
 
-const newList = initialList.map((value, index) => value * index);
+const newList = [];
+for (let i = 0; i < list.length; i++) {
+  newList.push(list[i] * i);
+}
 
 console.log(initialList);
 console.log(newList);
