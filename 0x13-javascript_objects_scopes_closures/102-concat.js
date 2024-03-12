1dbd5e6af3f42260f22b57a32d6dadaa440d5a8b
ZobaOkafor <judithokafor30@gmail.com>
2024-03-12 05:07:01 -0700

    JavaScript objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,20 +2,13 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.log('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-
 const fileA = process.argv[2];
 const fileB = process.argv[3];
-const fileC = process.argv[4];
+const destination = process.argv[4];
 
-const contentA = fs.readFileSync(fileA, 'utf8');
-const contentB = fs.readFileSync(fileB, 'utf8');
+const dataA = fs.readFileSync(fileA, 'utf8');
+const dataB = fs.readFileSync(fileB, 'utf8');
 
-const concatenatedContent = contentA + '\n' + contentB;
+const concatenatedData = dataA + dataB;
 
-fs.writeFileSync(fileC, concatenatedContent);
-
-console.log(`Concatenated ${fileA} and ${fileB} into ${fileC}`);
+fs.writeFileSync(destination, concatenatedData);
