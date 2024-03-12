179a088bec5e3f7d4a8cfe9dbbd953e9feaffa26
ZobaOkafor <judithokafor30@gmail.com>
2024-03-12 04:38:22 -0700

    JavaScript objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nb0ccutences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
+    if (searchElement === list[i]) {
       count++;
     }
   }
