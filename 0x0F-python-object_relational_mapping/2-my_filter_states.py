86bfc763a4af7242208dbeba8db9c5553fd789df
ZobaOkafor <judithokafor30@gmail.com>
2024-03-16 07:20:53 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,35 +8,17 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print(
-                "Usage: {} <username> <password> <database> <state_name>"
-                .format(sys.argv[0])
-                )
-        sys.exit(1)
-
-    username, password, database, state_name = sys.argv[1:]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
             host="localhost", port=3306,
-            user=username, passwd=password, db=database
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
             )
-
-    # Create a cursor object
     cursor = db.cursor()
-
-    # Execute the query
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all the rows
+    cursor.execute(
+                    "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+                    .format(sys.argv[4])
+    )
     rows = cursor.fetchall()
-
-    # Display the results
     for row in rows:
         print(row)
-
-    # Close the cursor and database connection
     cursor.close()
     db.close()
