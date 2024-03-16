23c1258beb3942a98d93328ddd4ed1addf9afb90
ZobaOkafor <judithokafor30@gmail.com>
2024-03-16 07:05:20 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,38 +1,57 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+Lists all states from the database hbtn_0e_0_usa.
 """
 
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
-            .format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database, state_name = sys.argv[1:]
+def filter_states(username, password, database, state_name):
+    """
+    Fetches and prints states from the specified database where name
+    matches the argument.
 
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+        state_name (str): Name of the state to search for.
+    """
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-            user=username, passwd=password, db=database)
-
-    # Create a cursor object
-    cursor = db.cursor()
+    db = MySQLdb.connect(
+                        host="localhost", user=username,
+                        passwd=password, db=database, port=3306
+        )
+    cur = db.cursor()
 
     # Execute the query
-    query = "SELECT * FROM states WHERE name = %s
-    ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
+    cur.execute("""SELECT * FROM states
+                WHERE name = %s ORDER BY states.id ASC""", (state_name,))
 
     # Fetch all the rows
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
 
     # Display the results
     for row in rows:
         print(row)
 
     # Close the cursor and database connection
-    cursor.close()
+    cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print(
+                "Usage: {} <username> <password> <database> <state_name>"
+                .format(sys.argv[0])
+                )
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    filter_states(username, password, database, state_name)
