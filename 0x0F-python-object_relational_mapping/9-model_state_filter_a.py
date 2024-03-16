88985e2f9bb48d1a6114289bbafcbbd8a1c9bd4a
ZobaOkafor <judithokafor30@gmail.com>
2024-03-16 14:03:24 -0700

    Lists all State objects that contain the letter a from the database

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,9 +13,8 @@ if __name__ == "__main__":
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost:3306/{}'
             .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).filter(State.name.like('%a%')):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+        print(instance.id, instance.name, sep=": ")
