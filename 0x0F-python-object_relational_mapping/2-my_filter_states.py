d39898db7f065c776b4a304840a0a15102cc460a
ZobaOkafor <judithokafor30@gmail.com>
2024-03-16 06:02:08 -0700

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, database, state_name):
+    """
+    Fetches and prints states from the specified database where name matches the argument.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+        state_name (str): Name of the state to search for.
+    """
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name = %s ORDER BY states.id ASC""", (state_name,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    filter_states(username, password, database, state_name)
