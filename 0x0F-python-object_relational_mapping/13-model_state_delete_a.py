3bb59ec81e838c54293ebcd6fb082386a356f481
ZobaOkafor <judithokafor30@gmail.com>
2024-03-17 10:11:04 -0700

    Delete all State objects with a name containing the letter a from the database

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states_to_delete = session.query(
-            (State).filter(State.name.like('%a%')).all())
+            (State).filter(State.name.like('%a%')))
     for state in states_to_delete:
         session.delete(state)
     session.commit()
