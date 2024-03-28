88d5200e72437712575daba1711ac051a3d6ea9f
ZobaOkafor <judithokafor30@gmail.com>
2024-03-28 14:37:11 -0700

    Whats my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status using the
+requests package and displays the body of the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    body = response.text
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
