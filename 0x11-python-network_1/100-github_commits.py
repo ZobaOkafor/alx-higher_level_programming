5e43e94e21cba1f4dcfdc988a019a584fbe900f0
ZobaOkafor <judithokafor30@gmail.com>
2024-03-28 15:32:48 -0700

    Interview question

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,18 @@ the GitHub API to display your id.
 
 import requests
 import sys
+import base64
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
+    # Combine username and password with a colon and encode in Base64
+    auth_header = base64.b64encode(f"{username}:{password}".encode()).decode()
+
+    headers = {'Authorization': f'Basic {auth_header}'}
     url = 'https://api.github.com/user'
-    response = requests.get(url, auth=(username, password))
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         user_data = response.json()
