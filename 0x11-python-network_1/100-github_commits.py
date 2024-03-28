9e20b4fe99dea08073ac5820c45b84eb66618d69
ZobaOkafor <judithokafor30@gmail.com>
2024-03-28 15:44:52 -0700

    Interview question

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,14 +9,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    repository = sys.argv[1]
-    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
-    response = requests.get(url)
-    commits = response.json()
-
-    for commit in commits:
-        sha = commit.get('sha')
-        author = commit.get('commit').get('author').get('name')
-        print(f"{sha}: {author}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
