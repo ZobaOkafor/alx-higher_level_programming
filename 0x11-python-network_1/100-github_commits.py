9118a2bf8be923d4f11991d78ce7b5e788990320
ZobaOkafor <judithokafor30@gmail.com>
2024-03-28 15:42:45 -0700

    Interview question

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,22 +7,16 @@ the GitHub API to display your id.
 
 import requests
 import sys
-import base64
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
+    repository = sys.argv[1]
+    owner = sys.argv[2]
 
-    # Combine username and password with a colon and encode in Base64
-    auth_header = base64.b64encode(f"{username}:{password}".encode()).decode()
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
+    commits = response.json()
 
-    headers = {'Authorization': f'Basic {auth_header}'}
-    url = 'https://api.github.com/user'
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data.get('id')
-        print(user_id)
-    else:
-        print(None)
+    for commit in commits:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author}")
