44b915c27a26b48a77358697c788b0e68ceff24b
ZobaOkafor <judithokafor30@gmail.com>
2024-05-02 13:29:15 -0700

    Say hello to everybody!

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-  $('#btn_translate').click(function() {
-    const languageCode = $('#language_code').val();
-    const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
-    
-    $.get(url, function(data) {
-      $('#hello').text(data.hello);
+$('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
     });
   });
 });
