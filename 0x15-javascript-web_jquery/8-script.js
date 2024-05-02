c20e12908d39252f7295ab9f554a3e68fe87970d
ZobaOkafor <judithokafor30@gmail.com>
2024-05-02 13:00:35 -0700

    Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,8 @@
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+$(document).ready(function() {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+    const movies = data.results;
+    movies.forEach(function(movie) {
+      $('<li>').text(movie.title).appendTo('UL#list_movies');
+    });
+  });
 });
